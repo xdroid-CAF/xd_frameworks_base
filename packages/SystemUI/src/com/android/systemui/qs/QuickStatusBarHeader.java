bc4d8ef2742571846b87d2919de37ef8ebcde8a8
Lars Greiss <kufikugel@googlemail.com>
2012-12-17 05:11:21 -0600

    SystemUI: statusbar clock date customization (1/2)
    This commit add support for:
    
    - Clock enable/disable
    - Clock position
    - Clock seconds
    - AM/PM styles multilang support
    - Date format
    - Date style
    
    @mydongistiny:
    -Ported to Pie
    -Updated Left/Right Clock
    -Fixed heads-up enabling left clock when disabled
    
    @spezi77:
    - Updated for Android10
    - Adapted for ContentObserver
    - Add back clock seconds (why not though?)
    - Add clock center/right position
    - Add date right/left position
    - Fixed "Clock is double after reboot or update"
    - Clock: Fix crash in formatted string with date on right
    - CollapsedStatusbar: Fix phantom spaces in status bar
    - Make date/clock view play nice with collapsed/expanded statusbar
    - Fixed "system icons appear twice on lockscreen"
    - Fixed "Clock appears despite that it has been set to hidden"
    - Play nice with collapsed/expanded statusbar
    - Play nice with heads-up notifications
    - Make settings strings consistent: "status_bar" -> "statusbar"
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -269,6 +269,7 @@ public class QuickStatusBarHeader extends RelativeLayout implements
 
         mClockView = findViewById(R.id.clock);
         mClockView.setOnClickListener(this);
+        mClockView.setQsHeader();
         mDateView = findViewById(R.id.date);
         mSpace = findViewById(R.id.space);
 
@@ -462,6 +463,7 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         if (mExpanded == expanded) return;
         mExpanded = expanded;
         mHeaderQsPanel.setExpanded(expanded);
+        mDateView.setVisibility(mClockView.isClockDateEnabled() ? View.INVISIBLE : View.VISIBLE);
         updateEverything();
     }
 
