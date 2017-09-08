07b0131a45102a48d43d0d9e90b0642143516731
ezio84 <brabus84@gmail.com>
2017-09-08 18:53:36 +0200

    FW, SystemUI: Allow to set QS panel columns and rows [1/2]
    QS customizer will respect choosen columns too
    
    Change-Id: I77b0452d15eaefd4e081cf34f258a71548f4dba5

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4974,6 +4974,30 @@ public final class Settings {
         public static final String LOCKSCREEN_LOCK_ICON = "lockscreen_lock_icon";
 
         /**
+         * How many rows to show in the qs panel when in portrait
+         * @hide
+         */
+        public static final String QS_ROWS_PORTRAIT = "qs_rows_portrait";
+
+        /**
+         * How many rows to show in the qs panel when in landscape
+         * @hide
+         */
+        public static final String QS_ROWS_LANDSCAPE = "qs_rows_landscape";
+
+        /**
+         * How many columns to show in the qs panel when in portrait
+         * @hide
+         */
+        public static final String QS_COLUMNS_PORTRAIT = "qs_columns_portrait";
+
+        /**
+         * How many columns to show in the qs panel when in landscape
+         * @hide
+         */
+        public static final String QS_COLUMNS_LANDSCAPE = "qs_columns_landscape";
+
+        /**
          * These are all public system settings
          *
          * @hide
