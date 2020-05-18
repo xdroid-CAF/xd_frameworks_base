851bbc26ebebbff83efcad53242f43582c21e7c9
rituj <ritujbeniwal@gmail.com>
2020-05-18 12:23:21 +0530

    SystemUI: BiometricPrompt: Adjust layout if FOD is present.
     * Force portrait mode if FOD is present.
    
     * Devices can override "biometric_dialog_fod_margin".
    
    Change-Id: I0b68b1ee6c5a8031deb195fbdb0b23c33b0ab590

--- packages/SystemUI/src/com/android/systemui/biometrics/AuthBiometricView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/AuthBiometricView.java
@@ -60,6 +60,8 @@ public abstract class AuthBiometricView extends LinearLayout {
 
     private static final String TAG = "BiometricPrompt/AuthBiometricView";
 
+    private static final String FOD = "vendor.lineage.biometrics.fingerprint.inscreen";
+
     /**
      * Authentication hardware idle.
      */
@@ -195,6 +197,8 @@ public abstract class AuthBiometricView extends LinearLayout {
 
     protected final PackageManager mPackageManager;
 
+    protected boolean mHasFod;
+
     /**
      * Delay after authentication is confirmed, before the dialog should be animated away.
      */
@@ -256,6 +260,8 @@ public abstract class AuthBiometricView extends LinearLayout {
         mInjector.mBiometricView = this;
 
         mPackageManager = context.getPackageManager();
+
+        mHasFod = mPackageManager.hasSystemFeature(FOD);
 
         mAccessibilityManager = context.getSystemService(AccessibilityManager.class);
 
@@ -553,6 +559,10 @@ public abstract class AuthBiometricView extends LinearLayout {
         mSavedState = savedState;
     }
 
+    public boolean getHasFod() {
+        return mHasFod;
+    }
+
     private void setTextOrHide(TextView view, String string) {
         if (TextUtils.isEmpty(string)) {
             view.setVisibility(View.GONE);
@@ -635,6 +645,28 @@ public abstract class AuthBiometricView extends LinearLayout {
             mTryAgainButton.setVisibility(View.GONE);
             Utils.notifyAccessibilityContentChanged(mAccessibilityManager, this);
         });
+
+        if (this instanceof AuthBiometricFingerprintView) {
+            if (mHasFod) {
+                final int navbarHeight = getResources().getDimensionPixelSize(
+                        com.android.internal.R.dimen.navigation_bar_height);
+                final int fodMargin = getResources().getDimensionPixelSize(
+                        R.dimen.biometric_dialog_fod_margin);
+
+                mIconView.setVisibility(View.INVISIBLE);
+                // The view is invisible, so it still takes space and
+                // we use that to adjust for the FOD
+                mIconView.setPadding(0, 0, 0, fodMargin - navbarHeight);
+
+                // Add Errortext above the biometric icon
+                this.removeView(mIndicatorView);
+                this.addView(mIndicatorView, this.indexOfChild(mIconView));
+            } else {
+                mIconView.setVisibility(View.VISIBLE);
+            }
+        } else if (this instanceof AuthBiometricFaceView) {
+            mIconView.setVisibility(View.VISIBLE);
+        }
     }
 
     /**
