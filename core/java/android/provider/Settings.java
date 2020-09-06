053d0cd7b84c54e10e7ff26e2c7f6322acb93a7d
cjybyjk <cjybyjk@zjnu.edu.cn>
2020-09-07 01:44:05 +0800

    FW, SystemUI: make app volume rows optional [1/2]
    Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>
    Change-Id: I5c02019a7cace127da41d72b386ad6f512871600
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5006,6 +5006,12 @@ public final class Settings {
         public static final String VOLUME_PANEL_ON_LEFT = "volume_panel_on_left";
 
         /**
+         * Show app volume rows in volume panel
+         * @hide
+         */
+        public static final String SHOW_APP_VOLUME = "show_app_volume";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
