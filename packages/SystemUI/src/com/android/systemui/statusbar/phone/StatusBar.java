8272fcbb6a14fae502a8951a8cc7f4c03d6dbe63
Rituj Beniwal <ritujbeniwal@gmail.com>
2021-06-13 13:34:01 +0530

    SystemUI: Brightness Slider: PA-ify
    * Move media player between brightness slider and footer button
    * Fix behaviour on landscape
    * Allow user more notification space in landscape
    * Fix brightness slider behaviour when swiping the QS panel or when swiping to show the statusbar
    * Switch PA footerview to new parent when switching tile layout
    * And some more
    
    Signed-off-by: Rituj Beniwal <ritujbeniwal@gmail.com>
    Change-Id: I11622eeba980fc8eb01c45f54c6ba4fae9a4a851

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -340,7 +340,6 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     private final VolumeComponent mVolumeComponent;
     private BrightnessMirrorController mBrightnessMirrorController;
-    private BrightnessMirrorController mQuickBrightnessMirrorController;
     private boolean mBrightnessMirrorVisible;
     private BiometricUnlockController mBiometricUnlockController;
     private final LightBarController mLightBarController;
@@ -1200,20 +1199,12 @@ public class StatusBar extends SystemUI implements DemoMode,
                         mBrightnessMirrorVisible = visible;
                         updateScrimController();
                     });
-            mQuickBrightnessMirrorController = new BrightnessMirrorController(
-                    mNotificationShadeWindowView,
-                    mNotificationPanelViewController,
-                    mNotificationShadeDepthControllerLazy.get(),
-                    (visible) -> {
-                        mBrightnessMirrorVisible = visible;
-                        updateScrimController();
-                    });
             fragmentHostManager.addTagListener(QS.TAG, (tag, f) -> {
                 QS qs = (QS) f;
                 if (qs instanceof QSFragment) {
                     mQSPanel = ((QSFragment) qs).getQsPanel();
                     mQSPanel.setBrightnessMirror(mBrightnessMirrorController);
-                    ((QuickStatusBarHeader)qs.getHeader()).getHeaderQsPanel().setBrightnessMirror(mQuickBrightnessMirrorController);
+                    ((QuickStatusBarHeader)qs.getHeader()).getHeaderQsPanel().setBrightnessMirror(mBrightnessMirrorController);
                 }
             });
         }
@@ -1416,9 +1407,6 @@ public class StatusBar extends SystemUI implements DemoMode,
         if (mBrightnessMirrorController != null) {
             mBrightnessMirrorController.onDensityOrFontScaleChanged();
         }
-        if (mQuickBrightnessMirrorController != null) {
-            mQuickBrightnessMirrorController.onDensityOrFontScaleChanged();
-        }
         // TODO: Bring these out of StatusBar.
         mUserInfoControllerImpl.onDensityOrFontScaleChanged();
         mUserSwitcherController.onDensityOrFontScaleChanged();
@@ -1445,9 +1433,6 @@ public class StatusBar extends SystemUI implements DemoMode,
         if (mBrightnessMirrorController != null) {
             mBrightnessMirrorController.onOverlayChanged();
         }
-        if (mQuickBrightnessMirrorController != null) {
-            mQuickBrightnessMirrorController.onOverlayChanged();
-        }
         // We need the new R.id.keyguard_indication_area before recreating
         // mKeyguardIndicationController
         mNotificationPanelViewController.onThemeChanged();
@@ -1458,9 +1443,6 @@ public class StatusBar extends SystemUI implements DemoMode,
     public void onUiModeChanged() {
         if (mBrightnessMirrorController != null) {
             mBrightnessMirrorController.onUiModeChanged();
-        }
-        if (mQuickBrightnessMirrorController != null) {
-            mQuickBrightnessMirrorController.onUiModeChanged();
         }
     }
 
@@ -2985,9 +2967,6 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
         if (mBrightnessMirrorController != null) {
             mBrightnessMirrorController.updateResources();
-        }
-        if (mQuickBrightnessMirrorController != null) {
-            mQuickBrightnessMirrorController.updateResources();
         }
     }
 
